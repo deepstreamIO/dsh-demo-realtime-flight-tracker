8b9b8eab9046a946394af373a1dbc4f982bc0fb8
Alex Harley <alexbruceharley@gmail.com>
2017-04-13 17:10:07 +0200

    real time updates working

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
 const deepstream = require('deepstream.io-client-js')
 const needle = require('needle')
 
-const records = new Map()
+let flightList
+const flights = new Map()
 
-const client = deepstream('')
+const client = deepstream('<Your app URL>')
 client.login({}, (success) => {
-  if (!success) throw new Error('Unable to connect to deepstreamHub')
+  flightList = client.record.getList('flights')
+  flightList.setEntries([])
   setInterval(fetchFlightData, 1000)
+  fetchFlightData()
+  setTimeout(() => setInterval(updateLocations, 500), 5000)
 })
 
-client.on('error', console.log)
-
-
-/**
- *0  icao24  string  Unique ICAO 24-bit address of the transponder in hex string representation.
-  1 callsign  string  Callsign of the vehicle (8 chars). Can be null if no callsign has been received.
-  2 origin_country  string  Country name inferred from the ICAO 24-bit address.
-  3 time_position float Unix timestamp (seconds) for the last position update. Can be null if no position report was received by OpenSky within the past 15s.
-  4 time_velocity float Unix timestamp (seconds) for the last velocity update. Can be null if no velocity report was received by OpenSky within the past 15s.
-  5 longitude float WGS-84 longitude in decimal degrees. Can be null.
-  6 latitude  float WGS-84 latitude in decimal degrees. Can be null.
-  7 altitude  float Barometric or geometric altitude in meters. Can be null.
-  8 on_ground boolean Boolean value which indicates if the position was retrieved from a surface position report.
-  9 velocity  float Velocity over ground in m/s. Can be null.
-  10  heading float Heading in decimal degrees clockwise from north (i.e. north=0°). Can be null.
-  11  vertical_rate float Vertical rate in m/s. A positive value indicates that the airplane is climbing, a negative value indicates that it descends. Can be null.
-  12  sensors int[] IDs of the receivers which contributed to this state vector. Is null if no filtering for sensor was used in the request.
- */
 const fetchFlightData = () => {
-  needle.get('https://opensky-network.org/api/states/all', (err, resp) => {
-    const { time, states } = resp.body
-    states.forEach((state) => {
-      if (!state[1] || !state[5] || !state[6]) return
-      updateLocation(state)
-    })
+  // flights around Berlin
+  needle.get('https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=52.74,52.19,12.91,13.74', (err, resp) => {
+    if (err) throw err
+    for (const key in resp.body) {
+      if (!(resp.body[key] instanceof Array)) continue
+      const [flightId, lat, lng] = resp.body[key]
+      addToBuffer(flightId, lat, lng)
+    }
   })
 }
 
-const updateLocation = (state) => {
-  console.log('Updating to', state[5], state[6])
-  let record = records.get(state[1])
-  process.nextTick(() => {
-    if (!record) {
-      record = client.record.getRecord(`flights/${state[1]}`)
-      records.set(`flights/${state[1]}`, record)
+const addToBuffer = (flightId, lat, lng) => {
+  const flightName = `flights/${flightId}`
+  let flight = flights.get(flightName)
+  if (!flight) {
+    flight = {}
+    flight.record = client.record.getRecord(flightName)
+    if (flightList.getEntries().indexOf(flightName) === -1) {
+      flightList.addEntry(flightName)
     }
+    flight.buffer = [{ lat, lng }]
+    flight.lastLocation = { lat, lng }
+    flights.set(flightName, flight)
+  }
+  if (flight.lastLocation.lat == lat && flight.lastLocation.lng == lng ) {
+    return
+  } else {
+    for (let i = 0; i < 50; i++) {
+      const newLat = lat - ((flight.lastLocation.lat - lat) / 50) * i
+      const newLng = lng - ((flight.lastLocation.lng - lng) / 50) * i
+      flight.buffer.push({ lat: newLat, lng: newLng })
+    }
+    flight.lastLocation = { lat, lng }
+  }
+}
 
-    record.set('lat', state[6])
-    record.set('lng', state[5])
-  })
+const updateLocations = () => {
+  for (const [flightName, flightData]  of flights) {
+    if (!flightData.buffer[0]) continue
+    flightData.record.set(
+      flightData.buffer.shift()
+    )
+  }
 }
